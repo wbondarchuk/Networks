e0d03ed325a7afaa833c8cfefe83fad1495835c7
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2020-12-07 14:26:20 +0700

    Update broadcastchat.py

--- broadcastchat.py
+++ broadcastchat.py
@@ -5,7 +5,6 @@ import queue
 
 def receiver(socket, message):
     while True:
-        # socket.recvfrom (bufsize) which returns us data and the address of the socket from which this data was received
         message, addr = socket.recvfrom(1024)
         print('from:', addr, ': ', message.decode())
 
@@ -17,17 +16,12 @@ def sender(socket, mes):
 
 
 # create listening socket
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# indicates that packets will be broadcast
 s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# allows multiple applications to "listen" to the socket
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# to listen, we connect to the address '0.0.0.0' - this means that all interfaces are listened to, 11719 - port_num
 s.bind(('0.0.0.0', 11719))
 
 # create flood broadcast network
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 mes = 'Hello!'
