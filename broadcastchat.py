177b6ac59f3fb1bc43ac3865d99d8819a79de710
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2020-12-07 17:24:50 +0700

    Update broadcastchat.py

--- broadcastchat.py
+++ broadcastchat.py
@@ -2,6 +2,16 @@ import socket
 from threading import Thread
 import queue
 
+# create listening socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('0.0.0.0', 11719))
+
+# create flood broadcast network
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+mes = 'Hello!'
 
 def receiver(socket, message):
     while True:
@@ -13,18 +23,6 @@ def sender(socket, mes):
     while True:
         str = input()
         socket.sendto(str.encode(), ('255.255.255.255', 11719))
-
-
-# create listening socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('0.0.0.0', 11719))
-
-# create flood broadcast network
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-mes = 'Hello!'
 
 thread_receive = Thread(target=receiver, args=(s, mes))
 thread_send = Thread(target=sender, args=(sock, mes))
